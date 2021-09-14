6397de7da87b72cda733e8cfd0a469d5915c090e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2021-09-14 17:59:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# stdpy
+ Java implementation of the places Python stdlib where it was determined to be more optimal or clean
---
+++ stdpy/src/test/java/io/github/gaming32/stdpy/BlockDequeTest.java
@@ -0,0 +1,9 @@
+package io.github.gaming32.stdpy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BlockDequeTest {
+    
+}
